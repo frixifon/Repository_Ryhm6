ead32656095cd0bbf058e6d456f776d6e27a533c
Opilane TTHK <opilane@tthk.ee>
2019-05-22 10:57:14 +0300

    gotovo

---
+++ git/joonis.js
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+	<title>Joonis</title>
+	<meta charset="utf-8">
+	<!--Удалить функции рисующие геометрические фигуры-->
+	<script type="text/javascript">
+	</script>
+</head>
+<body>
+<h1>Joonis</h1>
+<input type="button" value="Joonis" onClick="vasakjalg(), paremjalg(), keha(), rihm(), vasakkasi(), paremkasi(), pea(), silmad(), suu();">
+<input type="button" value="Kustuta" onClick="kustuta();">
+<br>
+<canvas id="plate" width="400" height="400" style= "background-color: white"></canvas>
+<br>
+<input type="button" value="Vasak jalg" onClick="vasakjalg();">
+<input type="button" value="Parem jalg" onClick="paremjalg();">
+<input type="button" value="Keha" onClick="keha();">
+<input type="button" value="Rihm" onClick="rihm();">
+<input type="button" value="Vasak Käsi" onClick="vasakkasi();">
+<input type="button" value="Parem Käsi" onClick="paremkasi();">
+<input type="button" value="Pea" onClick="pea();">
+<input type="button" value="Silmad" onClick="silmad();">
+<input type="button" value="Suu" onClick="suu();">
+
+<br>
+<br>
+<br>
+<br>
+<input type="button" value="Joon" onClick="joon();">
+<input type="button" value="Ruut" onClick="ruut();"></input>
+<input type="button" value="Ring" onClick="ring();">
+<input type="button" value="Kustuta" onClick="kustuta();">
+</body>
+
+</html>
